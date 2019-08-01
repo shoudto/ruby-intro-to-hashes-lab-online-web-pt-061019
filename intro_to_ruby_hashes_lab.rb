b6938c7c50ca7a4a0b59a486927a30e85d209d50
Oscar Shoudt <shoudto@gmail.com>
2019-08-01 19:14:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,8 +51,8 @@ def monopoly_with_fourth_tier
     :railroads => {
       :pieces => 4,
       :names => {
-        :reading_railroad => {"mortgage_value" => "$100"},
-        :pennsylvania_railroad => {},
+        :reading_railroad => {"mortgage_value" => '$100'},
+        :pennsylvania_railroad => {'mortgage_value' => '$200'},
         :b_and_o_railroad => {},
         :shortline => {}
       },
